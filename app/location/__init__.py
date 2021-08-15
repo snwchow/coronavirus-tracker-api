8863e2a10196f3d654ac3c83de93fcac0a41eae0
snwchow <85044004+snwchow@users.noreply.github.com>
2021-08-15 12:44:24 -0400

    Add files via upload
    (An attempt of) Implementing the Builder Design Pattern, generating csbs or nyt object using the BDP so that in the case where a new function is added in the future which doesn't require ALL the individual parts, the Builder can "cherry pick" the parts it needs and create a customized object for the new function to use, allowing greater flexibility.
    
    This could potentially reduce the number of parameters in the constructors to create the said customized csbs/nyt object, which would provide better readability, easier to read/understand what parameters the customized object has.
    
    Note that this (attempt of) implementation is meant to support the original functions, and that the implementation having LOTS of duplicated code (from csbs.py and nyt.py) suggests that the current implementation can be further refactored so that "code smell" can occur less.
    
    
    *regarding proper implementation*
    Location class and TimelinedLocation class in __init__.py should be removed as it is replaced by LocationBuilderInterface and TimelinedLocationBuilderInterface respectively as the Abstract interface.
    
    CSBSLocation class in csbs.py should be removed as it is replaced by CSBSLocationBuilder as the concrete Builder.
    similarly, NYTLocation with NYTLocationBuilder in nyt.py
    
    The Director and Product current located in csbs.py and nyt.py should be in their own respective module/file so that it is better organized and structured. (but it still works right now)
    
    The client code at the end of csbs.py and nyt.py should belong to another module/file where the other client codes are.
    
    The pseudo code of implementation have some lines of code commented out, which should reflect how it should looks like when properly implemented;
    Those lines of code are currently commented out to prevent program from unable to compile.

--- app/location/__init__.py
+++ app/location/__init__.py
@@ -2,9 +2,11 @@
 from ..coordinates import Coordinates
 from ..utils import countries
 from ..utils.populations import country_population
-
+from abc import ABCMeta
 
 # pylint: disable=redefined-builtin,invalid-name
+
+
 class Location:  # pylint: disable=too-many-instance-attributes
     """
     A location in the world affected by the coronavirus.
@@ -31,7 +33,6 @@ class Location:  # pylint: disable=too-many-instance-attributes
     def country_code(self):
         """
         Gets the alpha-2 code represention of the country. Returns 'XX' if none is found.
-
         :returns: The country code.
         :rtype: str
         """
@@ -41,7 +42,6 @@ class Location:  # pylint: disable=too-many-instance-attributes
     def country_population(self):
         """
         Gets the population of this location.
-
         :returns: The population.
         :rtype: int
         """
@@ -50,7 +50,6 @@ class Location:  # pylint: disable=too-many-instance-attributes
     def serialize(self):
         """
         Serializes the location into a dict.
-
         :returns: The serialized location.
         :rtype: dict
         """
@@ -101,7 +100,6 @@ class TimelinedLocation(Location):
     def serialize(self, timelines=False):
         """
         Serializes the location into a dict.
-
         :param timelines: Whether to include the timelines.
         :returns: The serialized location.
         :rtype: dict
@@ -122,3 +120,136 @@ class TimelinedLocation(Location):
 
         # Return the serialized location.
         return serialized
+
+
+class LocationBuilderInterface(metaclass=ABCMeta):  # pylint: disable=too-many-instance-attributes
+    """
+    The Abstract Location Builder Interface
+    """
+
+    def __init__(
+        self, id, country, province, coordinates, last_updated, confirmed, deaths, recovered,
+    ):  # pylint: disable=too-many-arguments
+        # General info.
+        self.id = id
+        self.country = country.strip()
+        self.province = province.strip()
+        self.coordinates = coordinates
+
+        # Last update.
+        self.last_updated = last_updated
+
+        # Statistics.
+        self.confirmed = confirmed
+        self.deaths = deaths
+        self.recovered = recovered
+
+    @property
+    def country_code(self):
+        """
+        Gets the alpha-2 code represention of the country. Returns 'XX' if none is found.
+        :returns: The country code.
+        :rtype: str
+        """
+        return (countries.country_code(self.country) or countries.DEFAULT_COUNTRY_CODE).upper()
+
+    @property
+    def country_population(self):
+        """
+        Gets the population of this location.
+        :returns: The population.
+        :rtype: int
+        """
+        return country_population(self.country_code)
+
+    def serialize(self):
+        """
+        Serializes the location into a dict.
+        :returns: The serialized location.
+        :rtype: dict
+        """
+        return {
+            # General info.
+            "id": self.id,
+            "country": self.country,
+            "country_code": self.country_code,
+            "country_population": self.country_population,
+            "province": self.province,
+            # Coordinates.
+            "coordinates": self.coordinates.serialize(),
+            # Last updated.
+            "last_updated": self.last_updated,
+            # Latest data (statistics).
+            "latest": {
+                "confirmed": self.confirmed,
+                "deaths": self.deaths,
+                "recovered": self.recovered,
+            },
+        }
+
+    # def build_part_a():
+    #     "Build part a, such as id"
+    #
+    # def build_part_b():
+    #     "Build part b, such as state"
+    # ...
+    # def get_result():
+    #     "Return final product"
+
+
+class TimelinedLocationBuilderInterface(LocationBuilderInterface):
+    """
+    The Abstract Timelined Location Builder Interface
+    """
+
+    # pylint: disable=too-many-arguments
+    def __init__(self, id, country, province, coordinates, last_updated, timelines):
+        super().__init__(
+            # General info.
+            id,
+            country,
+            province,
+            coordinates,
+            last_updated,
+            # Statistics (retrieve latest from timelines).
+            confirmed=timelines.get("confirmed").latest or 0,
+            deaths=timelines.get("deaths").latest or 0,
+            recovered=timelines.get("recovered").latest or 0,
+        )
+
+        # Set timelines.
+        self.timelines = timelines
+
+    # pylint: disable=arguments-differ
+    def serialize(self, timelines=False):
+        """
+        Serializes the location into a dict.
+        :param timelines: Whether to include the timelines.
+        :returns: The serialized location.
+        :rtype: dict
+        """
+        serialized = super().serialize()
+
+        # Whether to include the timelines or not.
+        if timelines:
+            serialized.update(
+                {
+                    "timelines": {
+                        # Serialize all the timelines.
+                        key: value.serialize()
+                        for (key, value) in self.timelines.items()
+                    }
+                }
+            )
+
+        # Return the serialized location.
+        return serialized
+
+    # def build_part_a():
+    #     "Build part a, such as id"
+    #
+    # def build_part_b():
+    #     "Build part b, such as state"
+    # ...
+    # def get_result():
+    #     "Return final product"
--- app/location/nyt.py
+++ app/location/nyt.py
@@ -1,5 +1,6 @@
 """app.locations.nyt.py"""
 from . import TimelinedLocation
+from . import TimelinedLocationBuilderInterface
 
 
 class NYTLocation(TimelinedLocation):
@@ -30,3 +31,68 @@ class NYTLocation(TimelinedLocation):
 
         # Return the serialized location.
         return serialized
+
+
+class NYTLocationBuilder(TimelinedLocationBuilderInterface):
+    """
+    The Concrete Builder for NYT
+    """
+
+    # pylint: disable=too-many-arguments,redefined-builtin
+    def __init__(self, id, state, county, coordinates, last_updated, timelines):
+        super().__init__(id, "US", state, coordinates, last_updated, timelines)
+
+        self.state = state
+        self.county = county
+        self.product = NYTProduct()
+
+    def serialize(self, timelines=False):  # pylint: disable=arguments-differ,unused-argument
+        """
+        Serializes the location into a dict.
+
+        :returns: The serialized location.
+        :rtype: dict
+        """
+        serialized = super().serialize(timelines)
+
+        # Update with new fields.
+        serialized.update(
+            {"state": self.state, "county": self.county,}
+        )
+
+        # Return the serialized location.
+        return serialized
+
+    # def build_part_a(self):
+    #     #this should actually puts the "part" inside the object, part being id, state, country, etc.
+    #     self.product.parts.append('additional parts')
+    #     return self
+
+class NYTDirector:
+    """
+    The Director, building a complex representation.
+    """
+    def construct():
+        return NYTLocationBuilder()#\
+            # # build and add part_a, ie. id; part_b, ie. state; etc.
+            # .build_part_a()
+
+
+class NYTProduct():
+    """
+    The Product
+    """
+    def __init__(self):
+        """
+        the NYT product should be empty when the init method is first called
+        containing a parts attribute
+        which is of type dictionary
+        """
+        self.parts = {}
+        pass
+
+
+# # The Client
+# #client code that initiates the construction process
+# #this should belong where all the other client codes are 
+# PRODUCT = NYTDirector.construct()
