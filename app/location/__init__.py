23ce21a12f1c60ebbca72499823972910fb8e923
snwchow <85044004+snwchow@users.noreply.github.com>
2021-08-15 22:19:46 -0400

    Add files via upload

--- app/location/__init__.py
+++ app/location/__init__.py
@@ -2,6 +2,7 @@
 from ..coordinates import Coordinates
 from ..utils import countries
 from ..utils.populations import country_population
+from abc import ABCMeta
 
 
 # pylint: disable=redefined-builtin,invalid-name
@@ -9,6 +10,7 @@ class Location:  # pylint: disable=too-many-instance-attributes
     """
     A location in the world affected by the coronavirus.
     """
+    #This would be the "Abstraction"
 
     def __init__(
         self, id, country, province, coordinates, last_updated, confirmed, deaths, recovered,
@@ -78,6 +80,129 @@ class TimelinedLocation(Location):
     """
     A location with timelines.
     """
+    #This would be the Extended Abstraction
+
+    # pylint: disable=too-many-arguments
+    def __init__(self, id, country, province, coordinates, last_updated, timelines):
+        super().__init__(
+            # General info.
+            id,
+            country,
+            province,
+            coordinates,
+            last_updated,
+            # Statistics (retrieve latest from timelines).
+            confirmed=timelines.get("confirmed").latest or 0,
+            deaths=timelines.get("deaths").latest or 0,
+            recovered=timelines.get("recovered").latest or 0,
+        )
+
+        # Set timelines.
+        self.timelines = timelines
+
+    # pylint: disable=arguments-differ
+    def serialize(self, timelines=False):
+        """
+        Serializes the location into a dict.
+
+        :param timelines: Whether to include the timelines.
+        :returns: The serialized location.
+        :rtype: dict
+        """
+        serialized = super().serialize()
+
+        # Whether to include the timelines or not.
+        if timelines:
+            serialized.update(
+                {
+                    "timelines": {
+                        # Serialize all the timelines.
+                        key: value.serialize()
+                        for (key, value) in self.timelines.items()
+                    }
+                }
+            )
+
+        # Return the serialized location.
+        return serialized
+
+
+# pylint: disable=redefined-builtin,invalid-name
+class LocationImplementor(metaclass=ABCMeta):  # pylint: disable=too-many-instance-attributes
+    """
+    A locationImplementor in the world affected by the coronavirus.
+    """
+    #This would be the Implementor
+
+    def __init__(
+        self, id, country, province, coordinates, last_updated, confirmed, deaths, recovered,
+    ):  # pylint: disable=too-many-arguments
+        # General info.
+        self.id = id
+        self.country = country.strip()
+        self.province = province.strip()
+        self.coordinates = coordinates
+
+        # Last update.
+        self.last_updated = last_updated
+
+        # Statistics.
+        self.confirmed = confirmed
+        self.deaths = deaths
+        self.recovered = recovered
+
+    @property
+    def country_code(self):
+        """
+        Gets the alpha-2 code represention of the country. Returns 'XX' if none is found.
+
+        :returns: The country code.
+        :rtype: str
+        """
+        return (countries.country_code(self.country) or countries.DEFAULT_COUNTRY_CODE).upper()
+
+    @property
+    def country_population(self):
+        """
+        Gets the population of this location.
+
+        :returns: The population.
+        :rtype: int
+        """
+        return country_population(self.country_code)
+
+    def serialize(self):
+        """
+        Serializes the location into a dict.
+
+        :returns: The serialized location.
+        :rtype: dict
+        """
+        return {
+            # General info.
+            "id": self.id,
+            "country": self.country,
+            "country_code": self.country_code,
+            "country_population": self.country_population,
+            "province": self.province,
+            # Coordinates.
+            "coordinates": self.coordinates.serialize(),
+            # Last updated.
+            "last_updated": self.last_updated,
+            # Latest data (statistics).
+            "latest": {
+                "confirmed": self.confirmed,
+                "deaths": self.deaths,
+                "recovered": self.recovered,
+            },
+        }
+
+
+class TimelinedLocationImplementor(LocationImplementor):
+    """
+    A location implementor with timelines.
+    """
+    #This would be an "extended" implementor
 
     # pylint: disable=too-many-arguments
     def __init__(self, id, country, province, coordinates, last_updated, timelines):
